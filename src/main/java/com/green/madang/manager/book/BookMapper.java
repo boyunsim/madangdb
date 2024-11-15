8b83430ff93a5af575479c752806a0b61e1bc9c0
boyunsim <tlaqhdbs12@gmail.com>
2024-11-15 18:08:25 +0900

    24-11-15

--- src/main/java/com/green/madang/manager/book/BookMapper.java
+++ src/main/java/com/green/madang/manager/book/BookMapper.java
@@ -4,7 +4,7 @@ import com.green.madang.manager.book.model.*;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
-
+//매퍼
 @Mapper
 public interface BookMapper {
     int insBook (BookPostReq p);
